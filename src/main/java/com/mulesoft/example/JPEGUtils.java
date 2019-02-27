1334d9a89a41b274502c9a2f479ccefb388a8760
dfelcey <dfelcey@dfelcey-ltm3>
2019-02-27 13:45:00 +0000

    Fixed some bugs in image compression

--- src/main/java/com/mulesoft/example/JPEGUtils.java
+++ src/main/java/com/mulesoft/example/JPEGUtils.java
@@ -11,7 +11,7 @@ import javax.imageio.ImageWriteParam;
 import javax.imageio.ImageWriter;
 
 public class JPEGUtils {
-	public static String CompressJpegImage(byte [] imageData) {
+	public static String CompressJpegImage(byte [] imageData, float com_factor) {
 		try {
 			ByteArrayInputStream bais = new ByteArrayInputStream(imageData);
 			BufferedImage image = ImageIO.read(bais);
@@ -20,7 +20,7 @@ public class JPEGUtils {
 
 			ImageWriteParam param = writer.getDefaultWriteParam();
 			param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-			param.setCompressionQuality(0.2f); // Change this, float between 0.0 and 1.0
+			param.setCompressionQuality(com_factor); // Change this, float between 0.0 and 1.0
 
 			writer.setOutput(ImageIO.createImageOutputStream(os));
 			writer.write(null, new IIOImage(image, null, null), param);
